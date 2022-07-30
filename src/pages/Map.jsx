3ed1544279fa5fef8d9ef285e0ef6d533fd927c6
Muneeb <muneebmughalr@gmail.com>
2022-07-30 22:59:18 +0500

    Maps

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -2,34 +2,27 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { MAPS_API_KEY } from "../config/config";
 const Map = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: MAPS_API_KEY,
+  });
   const User = useSelector((state) => state.user);
   if (!User.isLoggedIn) return <Navigate to="/login" />;
+
+  if (!isLoaded) return <div>Loading...</div>;
   return (
     <div>
       <Navbar />
-      <div
-        style={{
-          width: "80%",
-          border: "2px solid black",
-          marginLeft: "10%",
-          marginTop: "5%",
-          height: "65vh",
-        }}
-      >
-        <iframe
-          width="100%"
-          height="100%"
-          frameBorder="0"
-          scrolling="no"
-          marginheight="0"
-          marginwidth="0"
-          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=24.7136,46.6753+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+      <div style={{}}>
+        <GoogleMap
+          zoom={10}
+          center={{ lat: 24.7136, lng: 46.6753 }}
+          mapContainerClassName="map-container"
         >
-          <a href="https://www.maps.ie/distance-area-calculator.html">
-            area maps
-          </a>
-        </iframe>
+          <Marker position={{ lat: 24.7136, lng: 46.6753 }} />
+        </GoogleMap>
       </div>
     </div>
   );
